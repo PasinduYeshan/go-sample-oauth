46d03f9a7aeaf45ede6fa1ff94a8d577c00a7387
Pasindu Yeshan <pasinduyeshan98@gmail.com>
2025-03-02 16:39:23 +0530

    Add services api endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,6 +10,8 @@ import (
 func main() {
 	http.HandleFunc("/ads", handlers.GetAllAds)
 
+	http.HandleFunc("/services", handlers.GetAllServices)
+
 	log.Println("Server is running on port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("could not start server: %v", err)
